f8a26b2333f92518c9c6c7b88ad5e91abd722821
Hamza Elkotp <61008779+HamzaElkotp@users.noreply.github.com>
2021-12-22 14:02:59 +0300

    Generating content 4 z Blog page
    - Generating content for the Blog page
    - Fixing the date formatting
    - editing index.njk structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
+const { DateTime } = require("luxon");
+
+
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css'); // نستدعي باقي الملفات الي مجمعهاش
     eleventyConfig.addPassthroughCopy('./src/assets');
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
 
     return {
         dir: {
